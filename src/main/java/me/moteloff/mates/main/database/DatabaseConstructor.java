04892ae28ad866d4f152896ed3451f3e6fc3ce97
R1DD1 <76674487+R1DD1@users.noreply.github.com>
2023-02-13 22:12:48 +0600

    bump

--- src/main/java/me/moteloff/mates/main/database/DatabaseConstructor.java
+++ src/main/java/me/moteloff/mates/main/database/DatabaseConstructor.java
@@ -1,6 +1,7 @@
 package me.moteloff.mates.main.database;
 
 import me.moteloff.mates.main.Main;
+import org.bukkit.entity.Player;
 
 import java.sql.*;
 import java.util.Objects;
@@ -10,7 +11,7 @@ public class DatabaseConstructor {
 
     public static DatabaseConstructor INSTANCE = new DatabaseConstructor();
 
-    public String CREATE_PLAYER = "INSERT INTO 'mates' (`NickName`,`password`,`vkID`,`seen`) VALUES  (?,?,?,?)";
+    public String CREATE_PLAYER = "INSERT INTO 'mates' (`NickName`,`password`,`vkID`,`seen`, `class`) VALUES  (?,?,?,?,?)";
 
     public void createTable() {
         try {
@@ -18,7 +19,7 @@ public class DatabaseConstructor {
             if (conn != null) {conn.close();}
 
             conn = DriverManager.getConnection(Objects.requireNonNull(Main.getInstance().getConfig().getString("data_base_path")));
-            String CREATE_TABLES = "CREATE TABLE IF NOT EXISTS 'mates' (`id` INTEGER, `NickName` VARCHAR(16) NULL DEFAULT NULL , `password` VARCHAR(32) NULL DEFAULT NULL , `vkID` INT NULL DEFAULT NULL ,`seen` VARCHAR(200) NULL DEFAULT NULL , PRIMARY KEY (`id`));";
+            String CREATE_TABLES = "CREATE TABLE IF NOT EXISTS 'mates' (`id` INTEGER, `NickName` VARCHAR(16) NULL DEFAULT NULL , `password` VARCHAR(32) NULL DEFAULT NULL , `vkID` INT NULL DEFAULT NULL ,`seen` VARCHAR(200) NULL DEFAULT NULL, `class` VARCHAR(16) NULL DEFAULT NULL, PRIMARY KEY (`id`));";
             PreparedStatement statement = conn.prepareStatement(CREATE_TABLES);
             statement.executeUpdate();
             conn.close();
@@ -97,6 +98,20 @@ public class DatabaseConstructor {
         return null;
     }
 
+    public boolean updatePlayerClass(Player player, String playerClass) {
+        try {
+            conn = DriverManager.getConnection(Objects.requireNonNull(Main.getInstance().getConfig().getString("data_base_path")));
+            PreparedStatement statement = conn.prepareStatement("UPDATE 'mates' SET `class`=? WHERE NickName=?");
+            statement.setString(1, playerClass);
+            statement.setString(2, player.getName());
+            statement.executeUpdate();
+            conn.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     public boolean updateTimeSeen(String player, long seen) {
         try {
             conn = DriverManager.getConnection(Objects.requireNonNull(Main.getInstance().getConfig().getString("data_base_path")));
